cb3192c8e9b25023f6cdb3ff2a6d2387843a3c6e
Oleksandr_Shvets <oleksandr_shvets@epam.com>
2018-03-14 11:58:20 +0200

    added new files - preparation for homework2

---
+++ src/BublSortAlgr.java
@@ -0,0 +1,2 @@
+public class BublSortAlgr {
+}
---
+++ src/SevenPatternLoop.java
@@ -0,0 +1,9 @@
+public class SevenPatternLoop {
+    public static void main (String[] args) {
+        int [] vars = {1, 2, 3, 4, 5, 6, 7};
+
+        for (int i=0; i<vars.length;i++) {
+            System.out.println(vars[i]);
+        }
+    }
+}
